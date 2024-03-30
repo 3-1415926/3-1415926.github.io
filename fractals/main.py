40ed546bb7547182f7589f1a8fecb5b5fc67b8a8
Andrew Stasyuk <962690+3-1415926@users.noreply.github.com>
2024-03-29 22:47:25 -0700

    Added Sierpinsky square

--- fractals/main.py
+++ fractals/main.py
@@ -1,6 +1,5 @@
 import asyncio
 import numpy as np
-from pyodide.ffi import create_proxy
 from pyscript import document, window
 
 BYTES_PER_PIXEL = 4
@@ -12,22 +11,23 @@ def put_points(image_data, xs: np.ndarray, ys: np.ndarray):
     for x, y in zip(xs, ys):
         image_data.data[int(y) * image_data.width * BYTES_PER_PIXEL + int(x) * BYTES_PER_PIXEL + BYTES_PER_PIXEL - 1] = 255
 
-async def draw_sierpinsky(vert_x: list[float], vert_y: list[float], *, num_points=10000, num_iterations=100):
-    vert_x, vert_y = np.array(vert_x), np.array(vert_y)
+async def draw_sierpinsky(vert_x: list[float], vert_y: list[float], *, num_points=10000, num_iterations=100, w1=1, w2=1):
+    vert_x, vert_y = np.array(vert_x) * canvas.width, np.array(vert_y) * canvas.height
     assert len(vert_x) == len(vert_y)
     image_data = ctx.createImageData(canvas.width, canvas.height)
     x, y = None, None
     for i in range(num_iterations):
         vert_idx = np.random.randint(0, len(vert_x), num_points)
         attr_x, attr_y = vert_x[vert_idx], vert_y[vert_idx]
-        x, y = ((x + attr_x) / 2, (y + attr_y) / 2) if x is not None else (attr_x, attr_y)
+        if i > 0:
+            x, y = (w1 * x + w2 * attr_x) / (w1 + w2), (w1 * y + w2 * attr_y) / (w1 + w2)
+        else:
+            x, y = attr_x, attr_y
         put_points(image_data, x, y)
         ctx.putImageData(image_data, 0, 0)
         await asyncio.sleep(0)
 
-async def draw(*args):
-    await draw_sierpinsky(
-        [canvas.width      / 5, canvas.width  / 2, canvas.width  * 4 / 5],
-        [canvas.height * 4 / 5, canvas.height / 5, canvas.height * 4 / 5])
-    
-asyncio.ensure_future(draw())    
+asyncio.ensure_future(draw_sierpinsky(
+    # [0, .5, 1], [1,  0, 1]
+    [0, 0, 1, 1, .5,  1, .5, 0], [0, 1, 1, 0,  0, .5, 1, .5], w2=2
+))
